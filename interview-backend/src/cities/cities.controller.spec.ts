66d750acdac215fde5becd5dc4398cf688d0e31c
Sebas <toolt2y@gmail.com>
2023-08-02 15:02:13 +0200

    Added tests

--- interview-backend/src/cities/cities.controller.spec.ts
+++ interview-backend/src/cities/cities.controller.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CitiesController } from './cities.controller';
+import { CitiesService } from './cities.service';
+import * as CitiesJson from '../assests/cities.json';
 
 describe('CitiesController', () => {
-  let controller: CitiesController;
+  let citiesService: CitiesService;
+  let citiesController: CitiesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CitiesController],
+      providers: [CitiesService],
     }).compile();
 
-    controller = module.get<CitiesController>(CitiesController);
+    citiesService = module.get<CitiesService>(CitiesService);
+    citiesController = module.get<CitiesController>(CitiesController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(citiesController).toBeDefined();
+  });
+
+  describe('getCities', () => {
+    it('should return all cities', async () => {
+      const cities = await citiesController.getCities();
+      expect(cities).toBe(CitiesJson);
+    });
+  });
+
+  describe('getCitiesByName', () => {
+    it('should return cityName="Berlin" with "berlin" as parameter', async () => {
+      const response = await citiesController.getCitiesByName('berlin');
+      expect(response[0].cityName).toBe('Berlin');
+    });
+  });
+
+  describe('getCitiesByUuid', () => {
+    it('should return cityName="Stuttgard" with its uuid as parameter', async () => {
+      const response = await citiesController.getCitiesByUUID(
+        '66b8009b-319d-4272-92ea-853a10c27c9a',
+      );
+      expect(response[0].cityName).toBe('Stuttgart');
+    });
+  });
+
+  describe('getCitiesByCount', () => {
+    it('should return cityName="Essen" with 990 as parameter', async () => {
+      const response = await citiesController.getCitiesByCount('990');
+      expect(response[0].cityName).toBe('Essen');
+    });
   });
 });
--- interview-backend/src/cities/cities.service.spec.ts
+++ interview-backend/src/cities/cities.service.spec.ts
@@ -1,18 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CitiesService } from './cities.service';
+import * as CitiesJson from '../assests/cities.json';
 
 describe('CitiesService', () => {
-  let service: CitiesService;
+  let citiesService: CitiesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [CitiesService],
     }).compile();
 
-    service = module.get<CitiesService>(CitiesService);
+    citiesService = module.get<CitiesService>(CitiesService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(citiesService).toBeDefined();
+  });
+
+  describe('getCities', () => {
+    it('should return all cities', async () => {
+      const cities = await citiesService.getCities();
+      expect(cities).toBe(CitiesJson);
+    });
+  });
+
+  describe('getCitiesByName', () => {
+    it('should return cityName="Berlin" with "berlin" as name parameter', async () => {
+      const response = await citiesService.getCitiesByName('berlin');
+      expect(response[0].cityName).toBe('Berlin');
+    });
+  });
+
+  describe('getCitiesByUuid', () => {
+    it('should return cityName="Stuttgard" with its uuid as uuid parameter', async () => {
+      const response = await citiesService.getCitiesByUUID(
+        '66b8009b-319d-4272-92ea-853a10c27c9a',
+      );
+      expect(response[0].cityName).toBe('Stuttgart');
+    });
+  });
+
+  describe('getCitiesByCount', () => {
+    it('should return cityName="Essen" with 990 as count parameter', async () => {
+      const response = await citiesService.getCitiesByCount('990');
+      expect(response[0].cityName).toBe('Essen');
+    });
   });
 });
